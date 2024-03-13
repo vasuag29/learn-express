5ba733db912aa41309ee6cab2959da5bdfb7bd07
Vasu Agarwal <vasu@Vasus-MacBook-Pro.local>
2024-03-13 00:36:00 -0400

    Adding class activities.

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,22 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+router.get('/usernames', (req, res) => {
+  let users = req.users.map(user => ({ id: user.id, username: user.username }));
+  res.send(users);
+});
+
+router.get('/username/:name', (req, res) => {
+    const name = req.params.name;
+    const users = req.users.filter(person => person.username === name);
+
+    if (users.length === 0) {
+      console.log('User not found');
+      return res.status(404).send('User not found');
+    }
+    res.send(users);
+  });
+
+module.exports = router;
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -1,4 +1,21 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const router = express.Router()
+
+router.post('/adduser', (req, res) => {
+  let newUser = req.body;
+  req.users.push(newUser);
+  fs.writeFile(path.resolve(__dirname, '../data/users.json'), JSON.stringify(req.users), (err) => {
+    console.log(newUser);
+    if (err) {
+      console.log('Failed to write');
+      res.status(500).send('Failed to write user data');
+    } else {
+      console.log('User Saved');
+      res.send('User added successfully');
+    }
+  });
+});
+
+module.exports = router;
